e57ccb0394f85cc4dc4bdceac3c20baf1837e5d2
flaviodesousa <fl@viodesousa.com>
2009-09-21 01:56:51 +0000

    Incluidas tags para facilitar compressao do html configuracao do settings.py e master.html na geracao do deploy
    git-svn-id: svn+ssh://madelyn.moonlighting.com.br/var/svn/ratonator/trunk@6 29c57489-e0c1-4a52-aa39-cfb948b80a16

--- trunk/scripts/make_staging.sh
+++ trunk/scripts/make_staging.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
+temp=/tmp
 project_dir=ratonator
 source_path=~flavio/${project_dir}
 staging_path=~flavio/staging
 dateref=`date +'%Y%m%d%H%M'`
 source_backup=/tmp/backup-ratonator-${dateref}.tar.bz2
 deployment_archive=deploy-ratonator-${dateref}.tar.bz2
-deployment_path=/tmp
+deployment_path=${temp}
 shared_folder=/mnt/shared
-remote_target_address=
+
 
 cd ${source_path}
 
@@ -36,8 +37,15 @@ find ${staging_path} -name '.svn' -exec rm -rf \{\} +
 echo Deleting unwanted files...
 find ${staging_path} \( -name '*~' -o -name 'ratonator.e4p' -o -name '*po' -o -name '*.json' \) -delete -print
 
+echo "Patching for production"
+sed -e 's/DEBUG = True/DEBUG = False/' < ${staging_path}/${project_dir}/settings.py > ${temp}/settings.py-new &&
+    mv -f ${temp}/settings.py-new ${staging_path}/${project_dir}/settings.py
+sed -e 's/<!-- \(.*\)spaceless -->/{% \1spaceless %}/' < ${staging_path}/${project_dir}/templates/master.html > ${temp}/master.html-new &&
+    mv -f ${temp}/master.html-new ${staging_path}/${project_dir}/templates/master.html
+
 echo Packaging deployment
-tar jcf ${deployment_path}/${deployment_archive} ratonator
+cd ${staging_path} && \
+    tar jcf ${deployment_path}/${deployment_archive} ${project_dir}
 
 echo Sending to madelyn
 scp ${deployment_path}/${deployment_archive} flavio@madelyn.moonlighting.com.br:~/staging
